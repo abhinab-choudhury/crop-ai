a0e0ad7155298474aa4162aef451983fcb4341eb
Abhinab Choudhury <abhinabchoudhury291@gmail.com>
2025-09-10 19:10:35 +0000

    ml-model added

---
+++ ml-model/inference/plant-disease/onnx_inference.py
@@ -0,0 +1,83 @@
+from torchvision import transforms
+from PIL import Image
+from model import ResNet9
+import torch
+import io
+
+# Classes
+disease_classes = ['Apple___Apple_scab',
+                   'Apple___Black_rot',
+                   'Apple___Cedar_apple_rust',
+                   'Apple___healthy',
+                   'Blueberry___healthy',
+                   'Cherry_(including_sour)___Powdery_mildew',
+                   'Cherry_(including_sour)___healthy',
+                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                   'Corn_(maize)___Common_rust_',
+                   'Corn_(maize)___Northern_Leaf_Blight',
+                   'Corn_(maize)___healthy',
+                   'Grape___Black_rot',
+                   'Grape___Esca_(Black_Measles)',
+                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                   'Grape___healthy',
+                   'Orange___Haunglongbing_(Citrus_greening)',
+                   'Peach___Bacterial_spot',
+                   'Peach___healthy',
+                   'Pepper,_bell___Bacterial_spot',
+                   'Pepper,_bell___healthy',
+                   'Potato___Early_blight',
+                   'Potato___Late_blight',
+                   'Potato___healthy',
+                   'Raspberry___healthy',
+                   'Soybean___healthy',
+                   'Squash___Powdery_mildew',
+                   'Strawberry___Leaf_scorch',
+                   'Strawberry___healthy',
+                   'Tomato___Bacterial_spot',
+                   'Tomato___Early_blight',
+                   'Tomato___Late_blight',
+                   'Tomato___Leaf_Mold',
+                   'Tomato___Septoria_leaf_spot',
+                   'Tomato___Spider_mites Two-spotted_spider_mite',
+                   'Tomato___Target_Spot',
+                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                   'Tomato___Tomato_mosaic_virus',
+                   'Tomato___healthy']
+disease_model_path = './../../models/plant-disease/plant-disease-model.pth'
+
+# Device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load model
+disease_model = ResNet9(3, len(disease_classes))
+disease_model.load_state_dict(torch.load(disease_model_path, map_location=device))
+disease_model.to(device)
+disease_model.eval()
+
+def predict_image(img, model=disease_model):
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        # uncomment if training had normalization
+        # transforms.Normalize([0.485, 0.456, 0.406],
+        #                      [0.229, 0.224, 0.225])
+    ])
+    
+    if not isinstance(img, Image.Image):
+        img = Image.open(io.BytesIO(img)).convert("RGB")
+    else:
+        img = img.convert("RGB")
+    
+    img_t = transform(img).unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        yb = model(img_t)
+        _, preds = torch.max(yb, dim=1)
+    
+    return disease_classes[preds[0].item()]
+
+# Test
+img_path = "./image/test(1).JPG"
+img = Image.open(img_path).convert("RGB")
+prediction = predict_image(img)
+print("Prediction:", prediction)
