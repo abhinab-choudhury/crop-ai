0b19282274fff564386c4ee1a34827fabcf76488
Abhinab Sharma <146541054+Abhinab04@users.noreply.github.com>
2025-09-11 13:04:42 +0530

    fixed bug of langGraph.py

--- LangGraph/langGraphFile/langGraph.py
+++ LangGraph/langGraphFile/langGraph.py
@@ -3,60 +3,67 @@ import os
 from langchain.chat_models import init_chat_model
 import google.generativeai as genai
 
+#use ur Gemini api key
 genai.configure(api_key="")
 
-class GraphState(dict):
-    pass
+class GraphState(TypedDict, total=False):
+     input: str
+     route: Optional[str]
+     llm_output: Optional[str]
+     ml_output: Optional[str]
 
 def router_node(state: GraphState):
     query = state.get("input", "")
+    print("Router query:", query)
 
     prompt = f"""
     You are a router. The user may ask about:
-    - Crop rotation, soil, farming techniques → return 'LLM_Model'
-    - Plant disease, pest, or treatment (often with an image) → return 'ML_Model'
-    Respond ONLY with 'LLM_Model' or 'ML_Model'.
+    - Crop rotation, soil, farming techniques → return 'LLM_Node'
+    - Plant disease, pest, or treatment (often with an image) → return 'ML_Node'
+    Respond ONLY with 'LLM_Node' or 'ML_Node'.
     Query: {query}
     """
 
     response = genai.GenerativeModel("gemini-1.5-flash").generate_content(prompt)
-    print(response)
     decision = response.candidates[0].content.parts[0].text.strip().replace(" ", "")
+    
+    if decision not in ["LLM_Node", "ML_Node"]:
+        decision = "LLM_Node"
+
     state["route"] = decision
     return state
 
-def llm_node(state:GraphState):
-    state["llm_output"]="it will predict the which crop to plant in which moth for great revenue"
+# use LLM_Model ovr here and return it as a state
+def llm_node(state: GraphState):
+    state["llm_output"] = "It will predict which crop to plant in which month for great revenue."
+    return state
+# use ML_Model ovr here and return it as a state
+def ml_node(state: GraphState):
+    state["ml_output"] = "It will give me the result of which disease my plant has."
     return state
 
-def ml_node(state:GraphState): 
-    state["ml_output"]="it will give me the result of my plant has which dieases"
-    return state
+graph = StateGraph(GraphState)
 
-graph=StateGraph(GraphState)
-
-graph.add_node("Router_Node",router_node)
-graph.add_node("LLM_Node",llm_node)
-graph.add_node("ML_Node",ml_node)
-
+graph.add_node("Router_Node", router_node)
+graph.add_node("LLM_Node", llm_node)
+graph.add_node("ML_Node", ml_node)
 
 graph.add_edge(START, "Router_Node")
+
 graph.add_conditional_edges(
     "Router_Node",
     lambda state: state["route"],
     {
-        "LLM_Model": "LLM_Node",
-        "ML_Model": "ML_Node"
+        "LLM_Node": "LLM_Node",
+        "ML_Node": "ML_Node"
     }
 )
 
 graph.add_edge("LLM_Node", END)
 graph.add_edge("ML_Node", END)
 
-
+# helps us to compile the graph
 app = graph.compile()
 
-# result = app.invoke({"input": "Explain crop rotation"}, return_state=True)
-result = app.invoke({"input": "What disease does my plant have?"}, return_state=True)
-
+result = app.invoke({"input": "What crop rotation would yeild better revenue?"})
 print(result)
